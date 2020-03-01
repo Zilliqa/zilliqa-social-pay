1c87022ebaa807209974e3329a15bf1c542cf7c3
Rinat <figurative63@protonmail.com>
2020-03-01 13:37:00 +0000

    fixed colors

--- client/src/config/button-variants.ts
+++ client/src/config/button-variants.ts
@@ -1,8 +1,8 @@
 export enum ButtonVariants {
-  primary = 'background: #29CCC4;color: #FFFFFF;',
-  success = 'background: #2FA84F;color: #FFFFFF;',
-  info = 'background: #00B2FF;color: #FFFFFF;',
-  danger = 'background: #FA5D50;color: #FFFFFF;',
-  warning = 'background: #057A8E;color: #FFFFFF;',
-  outlet = 'background: transparent; border: 1px solid #29CCC4;color: #000000;'
+  primary = 'background: #29CCC4;color: #fff;',
+  success = 'background: #2FA84F;color: #fff;',
+  info = 'background: #00B2FF;color: #fff;',
+  danger = 'background: #FA5D50;color: #fff;',
+  warning = 'background: #057A8E;color: #fff;',
+  outlet = 'background: transparent; border: 1px solid #29CCC4;color: #242626;'
 };
