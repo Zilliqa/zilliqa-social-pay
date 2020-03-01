65dec4a1feb47c017f7f4540c7c8a7d10dff5853
Rinat <figurative63@protonmail.com>
2020-03-01 09:57:27 +0000

    added global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components'
 
 import { AppRoutes } from 'src/app-routes';
 
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <React.Fragment>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </React.Fragment>
   );
 }
 
