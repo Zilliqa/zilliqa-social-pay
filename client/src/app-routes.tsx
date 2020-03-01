d0253362ffe4fa4e0afbe4f8e1a42340bedb3479
Rinat <figurative63@protonmail.com>
2020-03-01 09:58:12 +0000

    fixed rounters

--- client/src/app-routes.tsx
+++ client/src/app-routes.tsx
@@ -3,9 +3,15 @@ import { Switch, Route } from 'react-router';
 
 import { AuthPage } from 'src/pages/auth';
 import { MainPage } from 'src/pages/main';
+import { Guide } from 'src/pages/guide';
 
 export const AppRoutes: React.FC = () => (
   <Switch>
+    <Route
+      path="/guide"
+      exact
+      component={Guide}
+    />
     <Route
       path="/auth"
       exact
---
+++ client/src/components/img.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+type Prop = {
+};
+
+export const Img = styled.img`
+`;
