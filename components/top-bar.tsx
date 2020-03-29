78ef2abd52abb3bc76954f999d148fc845b200dd
Rinat <figurative63@protonmail.com>
2020-03-29 16:23:24 +0000

    fixed tooltip

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -99,7 +99,7 @@ export const TopBar: React.FC = () => {
         fontVariant={Fonts.AvenirNextLTProBold}
         fontColors={FontColors.white}
         css="width: 200px;display: flex;align-items: center;"
-        data-tip={userState.synchronization ? 'Synchronization...' : 'Address configured.'}
+        data-tip={userState.synchronization ? 'Syncing address...' : 'Address configured.'}
         nowrap
       >
         {trimAddress}
