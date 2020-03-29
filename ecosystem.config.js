2af0cfa3e0007125bdecf45639c6c744c3af459c
Rinat <figurative63@protonmail.com>
2020-03-29 10:30:17 +0000

    fixed test env pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,20 +9,6 @@ module.exports = {
       JWT_SECRET: "SECRET",
       TWITTER_CONSUMER_KEY: "",
       TWITTER_CONSUMER_SECRET: "",
-      ADMIN_PRIVATE_KEY: "",
-      CONTRACT_ADDRESS: ""
-    }
-  }, {
-    name: "scheduler",
-    script: "node server/scheduler/index.js",
-    env: {
-      NODE_ENV: "production",
-      DEBUG: "zilliqa-social-pay:*",
-      SESSION: "session",
-      JWT_SECRET: "SECRET",
-      TWITTER_CONSUMER_KEY: "",
-      TWITTER_CONSUMER_SECRET: "",
-      ADMIN_PRIVATE_KEY: "",
       CONTRACT_ADDRESS: ""
     }
   }]
