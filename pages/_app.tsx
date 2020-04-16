4e369907b783680b4ca7c409f430e3b5b1c8ef76
Rinat <lich666black@gmail.com>
2020-04-16 10:54:04 +0000

    fixed mounted _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import App from 'next/app';
 
 import UserStore from 'store/user';
 import BrowserStore from 'store/browser';
+import EventStore from 'store/event';
 
 import { Container } from 'components/container';
 import { FixedWrapper } from 'components/fixed-wrapper';
@@ -209,16 +210,16 @@ class SocialPay extends App {
 
       if (!userState.jwtToken || !this.props.pageProps.user) {
         UserStore.clear();
-
-        if (this.props.router.route.includes('about')) {
-          return null;
-        } else if (this.props.router.route.includes('auth')) {
-          return null;
-        }
-
+        EventStore.signOut(null);
         this.props.router.push('/about');
       } else if (this.props.pageProps.user) {
         UserStore.setUser(this.props.pageProps.user);
+      }
+
+      if (this.props.router.route.includes('about')) {
+        return null;
+      } else if (this.props.router.route.includes('auth')) {
+        return null;
       }
     }
   }
