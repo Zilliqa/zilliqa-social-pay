1f23c0da70052fd5e939e1c2ea4eba1e2f4fe440
joel-zilliqa <joel@zilliqa.com>
2020-04-30 14:55:41 +0800

    CI integration initial commit.

---
+++ scripts/ci_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $(pwd)
+
+docker --version
+aws --version
+
+echo $TRAVIS_COMMIT
+commit=$(git rev-parse --short=7 $TRAVIS_COMMIT)
+
+accountID=$(aws sts get-caller-identity --output text --query 'Account')
+regionID=us-west-2
+
+application=socialpay-server
+registryURL=${accountID}.dkr.ecr.${regionID}.amazonaws.com/$application
+
+eval "$(aws ecr get-login --no-include-email --region $regionID)"
+docker build --build-arg DEPLOY_ENV="stg" -t "$registryURL:latest" -t "$registryURL:$commit" .
+docker push "$registryURL"
+
