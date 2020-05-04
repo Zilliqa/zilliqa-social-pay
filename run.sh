9f861f6534bc4f8df39a798781a141a1bee3ebad
joel-zilliqa <joel@zilliqa.com>
2020-05-04 21:58:25 +0800

    Added debug output to env variables.

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ echo "Environment is $DEPLOY_ENV"
 if [ "$DEPLOY_ENV" = "dev" ]; then
     npm run dev
 else
+    echo "Callback: $CALLBACK"
+    echo "Contract Address: $CONTRACT_ADDRESS"
     npm run db:create
     npm run start
 fi
