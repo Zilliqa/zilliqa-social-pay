9364446188896dc482de37f367b032dff33bae76
joel-zilliqa <joel@zilliqa.com>
2020-05-04 22:10:23 +0800

    Changed config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,10 @@ module.exports = {
     "storage": "storage.sql"
   },
   "test": {
-    "username": "postgres",
-    "password": "postgres",
-    "database": "database_social_pay",
-    "host": "127.0.0.1",
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "logging": false,
     "pool": {
