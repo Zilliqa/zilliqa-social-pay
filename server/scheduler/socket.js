ba97fa0a22b4e6f175c8cb762f05f4a0614cb302
Rinat <lich666black@gmail.com>
2020-07-23 18:00:45 +0500

    migrated to 3 tweets

--- server/scheduler/socket.js
+++ server/scheduler/socket.js
@@ -38,7 +38,6 @@ module.exports = async function (redisClient) {
 
       if (!Array.isArray(currenInfo.hashtags)) {
         currenInfo.hashtags = String(currenInfo.hashtags)
-          .replace('`', '')
           .split(',')
       }
 
