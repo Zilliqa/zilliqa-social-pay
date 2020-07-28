3d4706e7dab97cb605c2e10ec522db4cb817c6fc
Rinat <lich666black@gmail.com>
2020-07-28 18:37:36 +0500

    fxied blockchain updater

--- server/scheduler/blockchain.js
+++ server/scheduler/blockchain.js
@@ -10,26 +10,25 @@ const { blockchain } = models.sequelize.models;
 const log = bunyan.createLogger({ name: 'scheduler:blockchain' });
 
 module.exports = async function (redisClient) {
+  const blockchainInfo = await zilliqa.blockchainInfo();
+  const contractInfo = await zilliqa.getInit();
+  const { balance } = await zilliqa.getCurrentAccount(CONTRACT_ADDRESS);
+
+  let currenInfo = await blockchain.findOne({
+    where: { contract: CONTRACT_ADDRESS }
+  });
+
   try {
-    const blockchainInfo = await zilliqa.blockchainInfo();
-    const contractInfo = await zilliqa.getInit();
-    const { balance } = await zilliqa.getCurrentAccount(CONTRACT_ADDRESS);
-
-    let currenInfo = await blockchain.findOne({
-      where: { contract: CONTRACT_ADDRESS }
-    });
-
     if (!currenInfo) {
       log.warn('cannot find to blockchain info. currenInfo:', currenInfo, 'contracta address', CONTRACT_ADDRESS);
 
       await blockchain.create({
-        ...blockchainInfo,
         ...contractInfo,
         balance,
+        BlockNum: blockchainInfo.BlockNum,
+        DSBlockNum: blockchainInfo.DSBlockNum,
         rate: 60000,
         contract: CONTRACT_ADDRESS,
-        BlockNum: blockchainInfo.CurrentMiniEpoch,
-        DSBlockNum: blockchainInfo.CurrentDSEpoch,
         initBalance: balance
       });
 
@@ -44,8 +43,6 @@ module.exports = async function (redisClient) {
     }
 
     await currenInfo.update({
-      ...blockchainInfo,
-      ...contractInfo,
       balance,
       initBalance,
       BlockNum: blockchainInfo.CurrentMiniEpoch,
