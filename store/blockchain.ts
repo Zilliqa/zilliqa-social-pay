ce8edc631c6c4e8fd3b343226e195e6b64521c98
Rinat <lich666black@gmail.com>
2020-10-12 17:48:40 +0500

    change template tweet text

--- store/blockchain.ts
+++ store/blockchain.ts
@@ -52,14 +52,14 @@ const initalState: Blockchain = {
   initBalance: 0,
   campaignEnd: null,
   now: null,
-  hashtagText: `#Zil3 - Make It Count
-
-Do more with your dApps on #Zilliqa, the high-security, high-performance blockchain. 🌐
-
-Let’s celebrate this ecosystem with our community and guest-of-honour #CZBinance. 🚀
-
-🔽 Going live on 30th July, UTC 13:00 hrs
-https://youtu.be/vdREf5U9_X8`
+  hashtagText: `#Zilliqa's non-custodial staking is LIVE and I'm #StakingWithZIL 🚀
+ 
+  Join me and stake your $ZIL today! 🎉
+   
+  👉 zilliqa.com/staking
+   
+  #SocialStaking  
+  `
 };
 
 export const store = blockchainDomain.store<Blockchain>(initalState)
